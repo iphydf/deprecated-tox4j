af054bf615c90d316c8e9e01366f0451cb91e146
Nicolas Braud-Santoni <nicolas+tox@braud-santoni.eu>
2015-07-03 14:00:45 +0200

    Add FileLike trait and implementation
    - linters:
      - Temporarily disable EqualsNone:
        This wart has a bug and triggers on pattern-matching
      - Disable Override (it is broken)
    
    - build-extra: Disable type-inference warts
      Those warts often force code to use explicit types for everything
      (e.g. Map.empty is rejected by Wart.Nothing), and I do not believe
      this is helpful.
    
    - tox4j/build.sbt: Use scalaz-scalacheck-binding for tests

---
+++ projects/tox4j/src/main/java/im/tox/hlapi/storage/IOError.scala
@@ -0,0 +1,89 @@
+package im.tox.hlapi.storage
+
+import java.io.IOException
+import scala.util.Try
+import scalaz._
+
+sealed trait IOError
+
+/** Denotes an invalid argument documented by the method that returned it. */
+final case object InvalidArgument extends IOError
+
+/**
+ * Denotes an attempt to use a slice invalidated by `file.unsafeResize`.
+ *
+ * If the slice was invalidated, then the size extended to a larger size,
+ * whether this error is returned is implementation-defined.
+ */
+final case object InvalidSlice extends IOError
+
+/**
+ * Default error, in case no error more relevant was found.
+ *
+ * In particular, this is returned when an exception
+ * that is not an [[IOException]] was thrown.
+ */
+final case object UnknownFailure extends IOError
+
+/** Wraps an [[IOException]] as an [[IOError]] */
+final case class Exception(exn: IOException) extends IOError
+
+/** Helper functions to work with the `\/[IOError, _]` monad. */
+final object IOError {
+  /**
+   * Intercept exceptions and converts them to IOError.
+   *
+   * @returns `-\/(Exception(_))` if an [[IOException]] is intercepted,
+   *  else returns `-\/(UnknownFailure)` when intercepting another exception.
+   */
+  private def recover[A](tryValue: Try[\/[IOError, A]]): \/[IOError, A] = {
+    tryValue
+      .recover { case exn: IOException => -\/(Exception(exn)) }
+      .getOrElse(-\/(UnknownFailure))
+  }
+
+  /** Wraps an exception-raising expression into the [[IOError]] monad. */
+  def wrap[A](value: => A): \/[IOError, A] = {
+    recover(Try(value).map(\/-(_)))
+  }
+
+  /**
+   * Conditionally evaluates a total expression in the [[IOError]] monad.
+   *
+   * The expression must not throw exceptions.
+   *
+   * @param The condition under which the expression is evaluated.
+   * @param The [[IOError]] returned when [[condition]] is `false`.
+   * Defaults to [[InvalidArgument]]
+   * @param The expression which is evaluated when [[condition]] is `true`.
+   *
+   * @return `-\/(error)` if the condition is falsified, `\/-(value)` otherwise.
+   */
+  def condition[A](condition: Boolean, error: IOError = InvalidArgument)(value: => A): \/[IOError, A] = {
+    if (condition) {
+      \/-(value)
+    } else {
+      -\/(error)
+    }
+  }
+
+  /**
+   * Conditionally evaluates a partial expression in the [[IOError]] monad.
+   *
+   * The expression may return errors or throw exceptions.
+   * @param The condition under which the expression is evaluated.
+   * @param The [[IOError]] returned when [[condition]] is `false`.
+   * Defaults to [[InvalidArgument]]
+   * @param The expression which is evaluated when [[condition]] is `true`.
+   *
+   * @return `\/-(error)` if the condition is falsified, `value` if the evaluation
+   * didn't throw, and otherwise converts the exception to an [[IOError]].
+   */
+  def conditionWrap[A](condition: Boolean, error: IOError = InvalidArgument)(value: => \/[IOError, A]): \/[IOError, A] = {
+    if (condition) {
+      recover(Try(value))
+    } else {
+      -\/(error)
+    }
+  }
+}
