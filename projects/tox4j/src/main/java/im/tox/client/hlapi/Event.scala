6e886b4bc8e2890e1b4c290f30cceeddacb50c42
xxu52 <xxu52@mir.cs.illinois.edu>
2015-07-08 13:23:28 -0500

    Add ADT parameters

--- projects/tox4j/src/main/java/im/tox/client/hlapi/Event.scala
+++ projects/tox4j/src/main/java/im/tox/client/hlapi/Event.scala
@@ -13,9 +13,11 @@ object Event {
   //  Receive a chunk of file under transmission from friends
   case class ReceiveFileChunk()
   //  A friend’s connection status changes (online/offline)
-  case class ReceiveFriendConnectionStatus()
-  //  Receive a message from a friend/group
-  case class ReceiveMessage()
+  case class ReceiveFriendConnectionStatusChange()
+  //  Receive a message from a friend
+  case class ReceiveFriendMessage()
+  //  Receive a message from a group
+  case class ReceiveGroupMessage()
   //  A friend’s name changes
   case class ReceiveFriendNameChange()
   //  Receive a friend request
@@ -37,60 +39,62 @@ object Event {
    * GUI Events
    */
   //  Send a friend request
-  case class SendFriendRequest()
+  case class SendFriendRequest(friendId: String, request: String)
   //  Delete a friend
-  case class DeleteFriend()
+  case class DeleteFriend(friendId: String)
   //  See the details of a friend’s profile
-  case class RequestFriendProfile()
+  case class RequestFriendProfile(friendId: String)
   //  Change personal profile information
-  case class UpdateSelfProfile()
+  case class UpdateSelfProfile(profile: String)
   //  Change a friend’s alias
-  case class ChangeFriendAlias()
+  case class ChangeFriendAlias(friendId: String, newAlias: String)
   //  Change a group conversation’s alias
-  case class ChangeGroupConversationAlias()
+  case class ChangeGroupAlias(groupId: String, newAlias: String)
   //  Send a request to join a group
-  case class SendJoinGroupConversationRequest()
+  case class SendJoinGroupConversationRequest(groupId: String, request: String)
   //  Invite a friend to a group chat
-  case class InviteFriendToGroupConversationRequest()
+  case class InviteFriendToGroupRequest(groupId: String, friendId: String, request: String)
   //  Create a group chat
-  case class CreateGroupConversation()
+  case class CreateGroup()
   //  Initiate a conversation with a friend
-  case class CreatePrivateConversation()
+  case class CreatePrivateConversation(friendId: String)
+  //  Initiate a group conversation
+  case class CreateGroupConversation(groupId: String)
   //  Remove a member from a group chat
-  case class RemoveMemberFromGroupConversation()
+  case class RemoveMemberFromGroupConversation(groupId: String, friendId: String)
   //  Dismiss a group
-  case class DismissGroupConversation()
+  case class DismissGroupConversation(groupId: String)
   //  Leave a group conversation
-  case class LeaveGroupConversation()
+  case class LeaveGroupConversation(groupId: String)
   //  Delete a conversation
-  case class DeleteConversation()
+  case class DeleteConversation(conversationId: String)
   //  Send a text message to a conversation (group/private)
-  case class SendTextMessage()
+  case class SendTextMessage(conversationId: String, message: String)
   //  Initiate a file transmission request to a friend
-  case class SendFileTransmissionRequest()
+  case class SendFileTransmissionRequest(friendId: String, fileDescription: String)
   //  Get all conversations
   case class GetConversationList()
   //  Get all friends
   case class GetFriendList()
   //  Get the messages associated with a conversation
-  case class GetMessageList()
+  case class GetMessageList(conversationId: String)
   //  Get the file sent history with a friend
-  case class GetFileSentList()
+  case class GetFileSentList(conversationId: String)
   //  Login
-  case class Login()
+  case class Login(username: String, password: String)
   // Logout
   case class Logout()
   //  Set status message
-  case class ChangeStatusMessage()
+  case class ChangeStatusMessage(newStatusMessage: String)
   //  Change self user status
-  case class ChangeUserStatus()
+  case class ChangeUserStatus(status: String)
   //  Change self connection status
-  case class ChangeConnectionStatus()
+  case class ChangeConnectionStatus(status: String)
   //  Block/unblock a friend
-  case class ChangeFriendBlockStatus()
+  case class ChangeFriendBlockStatus(friendId: String)
   //  Mute/unmute a conversation
-  case class ChangeConversationMuteStatus()
+  case class ChangeConversationMuteStatus(friendId: String)
   //  Star/unstar a friend
-  case class ChangeConversationStarStatus()
+  case class ChangeConversationStarStatus(friendId: String)
 
 }
