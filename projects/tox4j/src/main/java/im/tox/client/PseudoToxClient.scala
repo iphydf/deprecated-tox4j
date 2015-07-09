3c326449b94ac7b2f1158dfedcf642b0f0d37f2e
xxu52 <xxu52@mir.cs.illinois.edu>
2015-07-08 19:24:04 -0500

    -

--- projects/tox4j/src/main/java/im/tox/client/PseudoToxClient.scala
+++ projects/tox4j/src/main/java/im/tox/client/PseudoToxClient.scala
@@ -15,35 +15,35 @@ abstract class PseudoToxClient {
   protected def handleNetworkEvent(e: NetworkEvent): Unit = {
     e match {
 
-        case ReceiveSelfConnectionStatus() =>
-        //  Receive file transmission control from friends
-        case ReceiveFileTransmissionControl() =>
-        //  Receive file transmission request from friends
-        case ReceiveFileTransmissionRequest() =>
-        //  Receive a chunk of file under transmission from friends
-        case ReceiveFileChunk() =>
-        //  A friend’s connection status changes (online/offline)
-        case ReceiveFriendConnectionStatusChange() =>
-        //  Receive a message from a friend
-        case ReceiveFriendMessage() =>
-        //  Receive a message from a group
-        case ReceiveGroupMessage() =>
-        //  A friend’s name changes
-        case ReceiveFriendNameChange() =>
-        //  Receive a friend request
-        case ReceiveFriendRequest() =>
-        //  A friend’s user status changes
-        case ReceiveFriendUserStatusChange() =>
-        //  A friend’s status message changes
-        case ReceiveFriendStatusMessageChange() =>
-        //  A friend typing status changes
-        case ReceiveFriendTypingStatusChange() =>
-        //  A lossy packet arrives
-        case ReceiveLossyPacket() =>
-        //  A lossless packet arrives
-        case ReceiveLosslessPacket() =>
-        //  Receive the read receipt of a message
-        case ReceiveReadReceipt() =>
+      case ReceiveSelfConnectionStatus() =>
+      //  Receive file transmission control from friends
+      case ReceiveFileTransmissionControl() =>
+      //  Receive file transmission request from friends
+      case ReceiveFileTransmissionRequest() =>
+      //  Receive a chunk of file under transmission from friends
+      case ReceiveFileChunk() =>
+      //  A friend’s connection status changes (online/offline)
+      case ReceiveFriendConnectionStatusChange() =>
+      //  Receive a message from a friend
+      case ReceiveFriendMessage() =>
+      //  Receive a message from a group
+      case ReceiveGroupMessage() =>
+      //  A friend’s name changes
+      case ReceiveFriendNameChange() =>
+      //  Receive a friend request
+      case ReceiveFriendRequest() =>
+      //  A friend’s user status changes
+      case ReceiveFriendUserStatusChange() =>
+      //  A friend’s status message changes
+      case ReceiveFriendStatusMessageChange() =>
+      //  A friend typing status changes
+      case ReceiveFriendTypingStatusChange() =>
+      //  A lossy packet arrives
+      case ReceiveLossyPacket() =>
+      //  A lossless packet arrives
+      case ReceiveLosslessPacket() =>
+      //  Receive the read receipt of a message
+      case ReceiveReadReceipt() =>
 
     }
   }
