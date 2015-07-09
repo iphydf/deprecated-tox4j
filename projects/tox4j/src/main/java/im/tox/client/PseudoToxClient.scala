e4201158828b0257df7d35bfc582250e016f2b98
xxu52 <xxu52@mir.cs.illinois.edu>
2015-07-09 11:12:07 -0500

    -

--- projects/tox4j/src/main/java/im/tox/client/PseudoToxClient.scala
+++ projects/tox4j/src/main/java/im/tox/client/PseudoToxClient.scala
@@ -14,100 +14,98 @@ final class PseudoToxClient {
   }
 
   private def handleNetworkEvent(state: ToxState, e: NetworkEvent): ToxState = {
-    state
-    /*e match {
+    e match {
 
-      case ReceiveSelfConnectionStatus()         =>
+      case ReceiveSelfConnectionStatus()         => state
       //  Receive file transmission control from friends
-      case ReceiveFileTransmissionControl()      =>
+      case ReceiveFileTransmissionControl()      => state
       //  Receive file transmission request from friends
-      case ReceiveFileTransmissionRequest()      =>
+      case ReceiveFileTransmissionRequest()      => state
       //  Receive a chunk of file under transmission from friends
-      case ReceiveFileChunk()                    =>
+      case ReceiveFileChunk()                    => state
       //  A friend’s connection status changes (online/offline)
-      case ReceiveFriendConnectionStatusChange() =>
+      case ReceiveFriendConnectionStatusChange() => state
       //  Receive a message from a friend
-      case ReceiveFriendMessage()                =>
+      case ReceiveFriendMessage()                => state
       //  Receive a message from a group
-      case ReceiveGroupMessage()                 =>
+      case ReceiveGroupMessage()                 => state
       //  A friend’s name changes
-      case ReceiveFriendNameChange()             =>
+      case ReceiveFriendNameChange()             => state
       //  Receive a friend request
-      case ReceiveFriendRequest()                =>
+      case ReceiveFriendRequest()                => state
       //  A friend’s user status changes
-      case ReceiveFriendUserStatusChange()       =>
+      case ReceiveFriendUserStatusChange()       => state
       //  A friend’s status message changes
-      case ReceiveFriendStatusMessageChange()    =>
+      case ReceiveFriendStatusMessageChange()    => state
       //  A friend typing status changes
-      case ReceiveFriendTypingStatusChange()     =>
+      case ReceiveFriendTypingStatusChange()     => state
       //  A lossy packet arrives
-      case ReceiveLossyPacket()                  =>
+      case ReceiveLossyPacket()                  => state
       //  A lossless packet arrives
-      case ReceiveLosslessPacket()               =>
+      case ReceiveLosslessPacket()               => state
       //  Receive the read receipt of a message
-      case ReceiveReadReceipt()                  =>
+      case ReceiveReadReceipt()                  => state
 
-    }*/
+    }
   }
 
   private def handleUiEvent(state: ToxState, e: UiEvent): ToxState = {
     e match {
 
-      case ChangeConnectionStatus(status)        => state.copy(connectionStatus = "blah")
-      case ChangeUserStatus(status)              => state.copy(userStatus = "blah")
+      case ChangeConnectionStatus(status) => state.copy(connectionStatus = "blah")
+      case ChangeUserStatus(status) => state.copy(userStatus = "blah")
       case ChangeStatusMessage(newStatusMessage) => state.copy(userProfile = state.userProfile.copy(nickName = "blah"))
-      case ChangeNickname(nickname)              => state.copy(userProfile = state.userProfile.copy(statusMessage = "blah"))
-      /*
-      case SendFriendRequest(friendId, request) =>
+      case ChangeNickname(nickname) => state.copy(userProfile = state.userProfile.copy(statusMessage = "blah"))
+      case SendFriendRequest(friendId, request) => state
       //  Delete a friend
-      case DeleteFriend(friendId) =>
+      case DeleteFriend(friendId) => state
       //  See the details of a friend’s profile
-      case RequestFriendProfile(friendId) =>
+      case RequestFriendProfile(friendId) => state
       //  Change a friend’s alias
-      case ChangeFriendAlias(friendId, newAlias) =>
+      case ChangeFriendAlias(friendId, newAlias) => state
       //  Change a group conversation’s alias
-      case ChangeGroupAlias(groupId, newAlias) =>
+      case ChangeGroupAlias(groupId, newAlias) => state
       //  Send a request to join a group
-      case SendJoinGroupConversationRequest(groupId, request) =>
+      case SendJoinGroupConversationRequest(groupId, request) => state
       //  Invite a friend to a group chat
-      case InviteFriendToGroupRequest(groupId, friendId, request) =>
+      case InviteFriendToGroupRequest(groupId, friendId, request) => state
       //  Create a group chat
-      case CreateGroup(groupName, option) =>
+      case CreateGroup(groupName, option) => state
       //  Initiate a conversation with a friend
-      case CreatePrivateConversation(friendId) =>
+      case CreatePrivateConversation(friendId) => state
       //  Initiate a group conversation
-      case CreateGroupConversation(groupId) =>
+      case CreateGroupConversation(groupId) => state
       //  Remove a member from a group chat
-      case RemoveMemberFromGroupConversation(groupId, friendId) =>
+      case RemoveMemberFromGroupConversation(groupId, friendId) => state
       //  Dismiss a group
-      case DismissGroupConversation(groupId) =>
+      case DismissGroupConversation(groupId) => state
       //  Leave a group conversation
-      case LeaveGroupConversation(groupId) =>
+      case LeaveGroupConversation(groupId) => state
       //  Delete a conversation
-      case DeleteConversation(conversationId) =>
+      case DeleteConversation(conversationId) => state
       //  Send a text message to a conversation (group/private)
-      case SendTextMessage(conversationId, message) =>
+      case SendTextMessage(conversationId, message) => state
       //  Initiate a file transmission request to a friend
-      case SendFileTransmissionRequest(friendId, fileDescription) =>
+      case SendFileTransmissionRequest(friendId, fileDescription) => state
       //  Get all conversations
-      case GetConversationList() =>
+      case GetConversationList() => state
       //  Get all friends
-      case GetFriendList() =>
+      case GetFriendList() => state
       //  Get the messages associated with a conversation
-      case GetMessageList(conversationId) =>
+      case GetMessageList(conversationId) => state
       //  Get the file sent history with a friend
-      case GetFileSentList(conversationId) =>
+      case GetFileSentList(conversationId) => state
       //  Login
-      case Login(username, password) =>
+      case Login(username, password) => state
       // Logout
-      case Logout() =>
+      case Logout() => state
       //  Block/unblock a friend
-      case ChangeFriendBlockStatus(friendId) =>
+      case ChangeFriendBlockStatus(friendId) => state
       //  Mute/unmute a conversation
-      case ChangeConversationMuteStatus(friendId) =>
+      case ChangeConversationMuteStatus(friendId) => state
       //  Star/unstar a friend
-      case ChangeConversationStarStatus(friendId) =>
-*/
+      case ChangeConversationStarStatus(friendId) => state
+
     }
   }
 
