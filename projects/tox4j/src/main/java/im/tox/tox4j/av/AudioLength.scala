96d23f6687497b52725c5d64e6231caec3f99c06
iphydf <iphydf@users.noreply.github.com>
2015-11-11 14:31:31 +0000

    Use Duration for AudioLength.

--- projects/tox4j/src/main/java/im/tox/tox4j/av/AudioLength.scala
+++ projects/tox4j/src/main/java/im/tox/tox4j/av/AudioLength.scala
@@ -1,22 +1,47 @@
 package im.tox.tox4j.av
 
-import im.tox.core.typesafe.DiscreteIntCompanion
+import java.io.{DataOutput, DataInputStream}
 
-final class AudioLength private (val value: Int) extends AnyVal
+import im.tox.core.error.DecoderError
+import im.tox.core.typesafe.{DiscreteValueCompanion, DiscreteIntCompanion}
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
+import scalaz.{\/-, -\/, \/}
+
+final class AudioLength private (private val value: Duration) extends AnyVal {
+  def toMicros: Int = value.toMicros.toInt
+}
 
 // scalastyle:off magic.number
-object AudioLength extends DiscreteIntCompanion[AudioLength](
-  2500, 5000, 10000, 20000, 40000, 60000
+object AudioLength extends DiscreteValueCompanion[AudioLength, Duration](
+  value => new AudioLength(value)
+)(
+  2500 microseconds,
+  5000 microseconds,
+  10000 microseconds,
+  20000 microseconds,
+  40000 microseconds,
+  60000 microseconds
 ) {
 
-  val Length2_5 = new AudioLength(2500)
-  val Length5 = new AudioLength(5000)
-  val Length10 = new AudioLength(10000)
-  val Length20 = new AudioLength(20000)
-  val Length40 = new AudioLength(40000)
-  val Length60 = new AudioLength(60000)
+  val Length2_5 = new AudioLength(values(0))
+  val Length5 = new AudioLength(values(1))
+  val Length10 = new AudioLength(values(2))
+  val Length20 = new AudioLength(values(3))
+  val Length40 = new AudioLength(values(4))
+  val Length60 = new AudioLength(values(5))
 
-  override def unsafeFromInt(value: Int): AudioLength = new AudioLength(value)
-  override def toInt(self: AudioLength): Int = self.value
+  override def write(self: AudioLength, packetData: DataOutput): Unit = {
+    packetData.writeShort(self.value.toMicros.toShort)
+  }
+
+  override def read(packetData: DataInputStream): \/[DecoderError, AudioLength] = {
+    val value = packetData.readUnsignedShort().microseconds
+    fromValue(value) match {
+      case None       => -\/(DecoderError.InvalidFormat(s"Invalid duration for $this: $value"))
+      case Some(self) => \/-(self)
+    }
+  }
 
 }
