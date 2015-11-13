82f7afa4316b848184d061046cfedc883e5bcada
iphydf <iphydf@users.noreply.github.com>
2015-11-13 16:09:38 +0000

    Reduce usage of unsafeFromByteArray.

--- projects/tox4j/src/test/java/im/tox/tox4j/core/callbacks/FriendRequestCallbackTest.scala
+++ projects/tox4j/src/test/java/im/tox/tox4j/core/callbacks/FriendRequestCallbackTest.scala
@@ -14,7 +14,7 @@ final class FriendRequestCallbackTest extends AliceBobTest {
     override def setup(tox: ToxCore[ChatState])(state: ChatState): ChatState = {
       tox.deleteFriend(AliceBobTestBase.FRIEND_NUMBER)
       if (isAlice) {
-        tox.addFriend(expectedFriendAddress, ToxFriendRequestMessage.unsafeFromByteArray(s"Hey this is $selfName".getBytes))
+        tox.addFriend(expectedFriendAddress, ToxFriendRequestMessage.fromString(s"Hey this is $selfName").get)
       }
       state
     }
